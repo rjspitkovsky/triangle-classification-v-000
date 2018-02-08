b6502b9d879247f6e703f5a2f50dc185ee0936de
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 21:42:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,10 +9,6 @@ class Triangle
   end
 
   def kind
-
-      raise TriangleError if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||(@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1)
-
-      
     if (@length1 == @length2) && (@length2 == @length3)
       return :equilateral
     end
@@ -24,12 +20,13 @@ class Triangle
     if @length1 != @length2 && @length2 != @length3 && @length1 != @length3
       return :scalene
     end
-
     # raise TriangleError if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||(@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1)
-
-
   end
 end
+
+  def valid?
+      raise TriangleError if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||(@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1)
+    end 
 
 
 class TriangleError < StandardError
