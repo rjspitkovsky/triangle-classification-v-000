c9f959537a17b8c75cdd125f3534043509d1e8a6
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 21:51:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,10 +25,6 @@ class Triangle
     if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||(@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1)
       begin
       raise TriangleError
-    rescue TriangleError => error
-      error.message
-    end
-
   end
 end
 
