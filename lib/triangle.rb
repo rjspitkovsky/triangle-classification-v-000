0e9507883627c538795846d91ad8aefaadc8b61b
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 21:51:15 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
 
 
     if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||(@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1)
-      begin 
+      begin
       raise TriangleError
     rescue TriangleError => error
       error.message
@@ -44,7 +44,5 @@ end
 
 
 class TriangleError < StandardError
-  def message
-    "This triangle violates triangle inequality."
-  end
+
 end
