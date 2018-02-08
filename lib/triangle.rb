44945a558ed1af07d6cb6482a804c69fa86a14d6
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 21:55:19 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,16 +12,15 @@ class Triangle
 
     if (@length1 == @length2) && (@length2 == @length3)
       return :equilateral
-    elsif 
+    elsif
        @length1 == @length2 || @length1 == @length3 || @length2 == @length3 && !(@length1 == @length2 && @length2 == @length3)
       return :isosceles
     elsif @length1 != @length2 && @length2 != @length3 && @length1 != @length3
       return :scalene
-    else 
+    else
 
 
-    # if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||(@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1)
-      begin
+    # if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||(@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1
       raise TriangleError
     end
      end
