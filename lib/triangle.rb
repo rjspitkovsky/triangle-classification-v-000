825d24bd908228e5fbb14b975d51ce011f48093f
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 21:48:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,19 +21,20 @@ class Triangle
       return :scalene
     end
 
-    if self.invalid?
+    if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||(@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1)
       raise TriangleError
     rescue TriangleError => error
-      puts error.message
-  
+      error.message
+    end 
+  end
 
 
   end
 
 
-  def self.invalid?
-      (@length1 == 0 || @length2 == 0 || @length3 == 0) || (@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1)
-    end
+  # def self.invalid?
+  #     (@length1 == 0 || @length2 == 0 || @length3 == 0) || (@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1)
+  #   end
 end
 
 class TriangleError < StandardError
