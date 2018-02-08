2992d04d6d2452b9997e310c74ea4403b8448b6d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 21:41:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,7 @@ class Triangle
     end
 
     raise TriangleError if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||(@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1)
-    end
+    
 
   end
 end
