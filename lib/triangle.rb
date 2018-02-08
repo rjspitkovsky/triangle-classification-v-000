575e7eeb448322bbac3d424170d2db480add0d58
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 21:33:17 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -29,11 +29,15 @@ class Triangle
     else
       return :scalene
     end
+    if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||
+      (@length1 + @length2 > @length3 || @length1 + @length3 > @length2 ||
+      @length2 + @length3 > @length1)
+      raise TriangleError
   end
-  if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||
-    (@length1 + @length2 > @length3 || @length1 + @length3 > @length2 ||
-    @length2 + @length3 > @length1)
-    raise TriangleError
+  # if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||
+  #   (@length1 + @length2 > @length3 || @length1 + @length3 > @length2 ||
+  #   @length2 + @length3 > @length1)
+  #   raise TriangleError
   end
 end
 
