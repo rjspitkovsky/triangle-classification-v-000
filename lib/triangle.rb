7fe8b266cbb918ce20414155b640e883b542fb18
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 22:00:58 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,9 +20,9 @@ class Triangle
     else
 
 
-    # if (@length1 == 0 || @length2 == 0 || @length3 == 0) ||(@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1
+     (self.length1 == 0 || self.length2 == 0 || self.length3 == 0) ||(self.length1 + self.length2 > self.length3 || self.length1 + self.length3 > self.length2 || self.length2 + self.length3 > self.length1)
       raise TriangleError
-    
+
      end
   end
 end
