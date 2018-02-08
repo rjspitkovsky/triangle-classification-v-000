c1380c672b3230c31928682248280c8b42b2f532
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 21:25:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,20 +1,29 @@
 class Triangle
 
-  attr_accessor :length1, :length2, :length3 
+  attr_accessor :length1, :length2, :length3
 
   def initialize(length1, length2, length3)
-    @length1 = length1 
-    @length2 = length2 
-    @length3 = length3 
-  end 
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
 
-  def kind 
-    if @length1 == @length2 && @length2 == @length3 
-      return :equilateral 
+  def kind
+    if (@length1 == 0 || @length2 == 0 || @length3 == 0) || 
+      (@length1 + @length2 > @length3 || @length1 + @length3 > @length2 ||
+      @length2 + @length3 > @length1) 
+      raise TriangleError      
+    elsif @length1 == @length2 && @length2 == @length3
+      return :equilateral
     elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3 && !(@length1 == @length2 && @length2 == @length3)
-      return :isosceles 
-    else 
-      return :scalene 
+      return :isosceles
+    else
+      return :scalene
     end 
-  
+
 end
+
+
+class TriangleError < StandardError
+end 
+    
