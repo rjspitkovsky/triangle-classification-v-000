74c04722d89c10e0150265dfe33f96fdbb2a63cb
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 22:06:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -28,16 +28,5 @@ class Triangle
 end
 
 
-
-
-
-
-
-
-  # def self.invalid?
-  #     (@length1 == 0 || @length2 == 0 || @length3 == 0) || (@length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1)
-  #   end
-
-
 class TriangleError < StandardError
 end
